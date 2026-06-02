384f150ff4255918d3adb03b04a805cac64ebfc3
Даниил Калашников <ddkalashnikov@edu.hse.ru>
2026-06-02 12:54:46 +0300

    Initial commit

---
+++ src/main/java/ru/codzilla/artefactik/artefactik0/Artefactik0Application.java
@@ -0,0 +1,13 @@
+package ru.codzilla.artefactik.artefactik0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Artefactik0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Artefactik0Application.class, args);
+    }
+
+}
